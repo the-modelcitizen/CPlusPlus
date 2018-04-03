e8818c00a60be4a436f3bf65ac73677a8a60ca8c
Nathan Mahoney <nortmahoney@gmail.com>
2018-04-02 20:35:39 -0400

    adding a simple application that finds max, min and average for numbers under 100

---
+++ Application2/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+    int userInput = 0;
+    int maxNumber = 0;
+    int minNumber = 100;
+    int sumTotal = 0;
+    float average = 0;
+
+    //this part gets the 15 requests and stores the 15 numbers from the user
+    for(int i = 0; i < 15; i++)
+    {
+        std::cout << "Enter a number between 0 and 100: ";
+        scanf("%d", &userInput);
+        std::cout << userInput << "\n";
+
+        if(userInput > maxNumber)
+        {//largest number entered by user becomes maxNumber when entered
+            maxNumber = userInput;
+        }
+        if(userInput < minNumber)
+        {//smallest number entered by user becomes minNumber when entered
+            minNumber = userInput;
+        }
+        sumTotal = sumTotal + userInput;
+    }
+    std::cout << "Maximum number = " << maxNumber << "\n";
+    std::cout << "Minimum number = " << minNumber << "\n";
+    average = sumTotal / 15;
+    std::cout << "Average = " << average << "\n";
+    return 0;
+}
